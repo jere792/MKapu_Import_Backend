6611dcd5e00761a459f8f3ef7c811b9add4409ff
Juan Diego <juandiego.gr05@gmail.com>
2026-02-12 01:11:37 -0500

    xd

--- apps/logistics/src/core/warehouse/inventory/infrastructure/adapters/in/controllers/inventory-message.controller.ts
+++ apps/logistics/src/core/warehouse/inventory/infrastructure/adapters/in/controllers/inventory-message.controller.ts
@@ -4,15 +4,14 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class InventoryMessageController {
-  
   @MessagePattern({ cmd: 'register_movement' })
   async handleRegisterMovement(@Payload() data: any) {
     console.log('📥 ¡TCP REQUEST RECIBIDO EN LOGISTICS!', data);
-    
+
     // Por ahora retornamos un OK para que Sales no explote
-    return { 
-      status: 'success', 
-      message: 'Movimiento procesado en inventario' 
+    return {
+      status: 'success',
+      message: 'Movimiento procesado en inventario',
     };
   }
 }
